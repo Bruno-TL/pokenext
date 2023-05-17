22fee3d3d392d869641aa001d29f6469b1f7160a
Thiago <tbgt35@hotmail.com>
2023-05-17 10:28:52 -0300

    atualizando arquivos

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './home.module.css'
 
 async function getStaticProps() {
   const api = 'https://pokeapi.co/api/v2/pokemon'
@@ -14,15 +15,15 @@ export default async function Home() {
   const pokemons = data.results;
   return (
     <>
-      <div>
-        <h1>PokeNext</h1>
-        <ul>
-          {pokemons.map((pokemon) => (
-            <li key={pokemon.id}>{pokemon.name}</li>
-          ))}
-        </ul>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>Poke<span>Next</span></h1>
+        <Image src="/images/pokeball.png" width="50" height="50" alt="poke bola" />
       </div>
-
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <p key={pokemon.id}>{pokemon.name}</p>
+        ))}
+      </div>
     </>
   )
 }
